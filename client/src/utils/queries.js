08208823a479ea3bf8bb4c5f495d486b9526ea32
Wanipopota <guanipag680@gmail.com>
2024-09-14 16:40:50 -0400

    re-estructuring client folders

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,65 +1,55 @@
-import gql from 'graphql-tag';
+import { gql } from '@apollo/client';
 
 export const QUERY_PRODUCTS = gql`
-	query getProducts($category: ID) {
-		products(category: $category) {
-			_id
-			name
-			description
-			price
-			quantity
-			image
-			
-		}
-	}
-`;
-
-export const QUERY_ALL_PRODUCTS = gql`
-	{
-		products {
-			_id
-			name
-			description
-			price
-			quantity
-		}
-	}
-`;
-
-export const QUERY_CATEGORIES = gql`
-	{
-		categories {
-			_id
-			name
-		}
-	}
-`;
-
-export const QUERY_USER = gql`
-	{
-		user {
-			firstName
-			lastName
-			orders {
-				_id
-				purchaseDate
-				products {
-					_id
-					name
-					description
-					price
-					quantity
-					image
-				}
-			}
-		}
-	}
+  query getProducts {
+    products {
+      _id
+      name
+      description
+      price
+      quantity
+      image
+    }
+  }
 `;
 
 export const QUERY_CHECKOUT = gql`
-	query getCheckout($products: [ID]!) {
-		checkout(products: $products) {
-			session
-		}
-	}
+  query getCheckout($products: [ProductInput]) {
+    checkout(products: $products) {
+      session
+    }
+  }
+`;
+
+export const QUERY_ALL_PRODUCTS = gql`
+  {
+    products {
+      _id
+      name
+      description
+      price
+      quantity
+    }
+  }
+`;
+
+export const QUERY_USER = gql`
+  {
+    user {
+      firstName
+      lastName
+      orders {
+        _id
+        purchaseDate
+        products {
+          _id
+          name
+          description
+          price
+          quantity
+          image
+        }
+      }
+    }
+  }
 `;
